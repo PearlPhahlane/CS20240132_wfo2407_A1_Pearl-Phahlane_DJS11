447dd4031e97220be072f878227f1a6b4372cdaa
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-21 20:56:25 +0200

    add media player in the header, and add it's css styling

--- cast-ring-app/src/components/Header.jsx
+++ cast-ring-app/src/components/Header.jsx
@@ -4,6 +4,12 @@ import { NavLink} from "react-router-dom";
 export default function Header() {
   return (
     <header>
+      <div className="media-player">
+        <audio controls>
+          <source src="your-audio-file.mp3" type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
       <nav>
         <NavLink to="/home">Home</NavLink>
         <NavLink to="/browse">Browse</NavLink>
