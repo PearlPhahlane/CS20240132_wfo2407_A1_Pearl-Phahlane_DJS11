c7a19ae388311160d7f15d0d5cb7c2bb99dd338c
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-25 09:16:33 +0200

    change name of MediaPlayer to PodcastPlayer, add css to be imported for styling the player

---
+++ cast-ring-app/src/components/PodcastPlayer.jsx
@@ -0,0 +1,10 @@
+import { useState, useRef, useEffect} from "react";
+import {
+    Play,
+    Pause,
+    Volume2,
+    VolumeX,
+    Settings,
+    Rewind,
+    FastForward
+} from "lucide-react";
