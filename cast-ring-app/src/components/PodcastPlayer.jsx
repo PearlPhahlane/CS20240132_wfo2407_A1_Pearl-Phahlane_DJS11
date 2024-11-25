346606ab77137bd989eec38448651e79cbee4076
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-25 09:53:58 +0200

    create function PodcastPlayer with parameters, inside function define the UI elements of the player and store each in a state, two more in a useRef, add event listners and remove event listners

--- cast-ring-app/src/components/PodcastPlayer.jsx
+++ cast-ring-app/src/components/PodcastPlayer.jsx
@@ -8,3 +8,42 @@ import {
     Rewind,
     FastForward
 } from "lucide-react";
+
+export default function PodcastPlayer({ 
+  audioUrl, 
+  episodeTitle = "Unknown Episode", 
+  showName = "Unknown Show",
+  episodeImage = "/api/placeholder/60/60"
+}) {
+    const[isPlaying, setIsPlaying] = useState(false);
+    const[duration, setDuration] = useState(0);
+    const[currentTime, setCurrentTime] = useState(0);
+    const[volume, setVolume] = useState(1);
+    const[isMute, setMute] = useState(false);
+    const[playbackRate, setPlayBackRate] = useState(1);
+
+    const audioRef = useRef(null);
+    const progressBarRef = useRef(null);
+
+    useEffect(() => {
+        const audio = audioRef.current;
+
+        const handleLoadedMetaData = () => {
+            setDuration(audio.duration);
+        };
+
+        const handleTimeUpdate = () => {
+            setCurrentTime(audio.currentTime);
+        };
+
+        audio.addEventListener('loadedmetadata', handleLoadedMetaData);
+        audio.addEventListener('timeupdate', handleTimeUpdate);
+
+        return () => {
+            audio.removeEventListner('loadedmetadata', handleLoadedMetaData);
+            audio.removeEventListner('timeupdate', handleTimeUpdate);
+        };
+    }, []);
+
+    
+}
