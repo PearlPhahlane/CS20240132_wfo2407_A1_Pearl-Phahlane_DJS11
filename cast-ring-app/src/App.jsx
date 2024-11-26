ccbe53d924bec9e695806055bd54858a3ad83207
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-26 13:53:58 +0200

    add landing page content, create landingPage.css file, add basic styling and import it to LandingPage.jsx

--- cast-ring-app/src/App.jsx
+++ cast-ring-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import Browse from "./pages/Browse";
 import Favorites from "./pages/Favorites";
@@ -11,6 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />}/>
         <Route path="/" element={<Layout />}>
          <Route path="home" element={<Home />} />
          <Route path="browse" element={<Browse />} />
--- cast-ring-app/src/pages/LandingPage.jsx
+++ cast-ring-app/src/pages/LandingPage.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import "./landingPage.css";
 
 export default function LandingPage() {
-    return(
-        <div>
-            <h1></h1>
-            <p>
-            </p>
-            <Link></Link>
-        </div>
+    return (
+      <div className="landing-container">
+        <h1>Tune In. Listen Up. Stay Inspired.</h1>
+        <p>
+          Explore our selection of podcasts, from true crime to comedy, tech to
+          wellness—your next favorite show is just a click away. Start listening
+          now and let the stories unfold!
+        </p>
+        <Link>Start Listening Now</Link>
+      </div>
     );
 }
