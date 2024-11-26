ca6eb53ad1e77eccd8bfd3bb003276e91b408bac
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-26 13:08:09 +0200

    add LandingPage.jsx and image for hero section on landing page

---
+++ cast-ring-app/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function {
+    return(
+        <div>
+            <h1></h1>
+            <p>
+            </p>
+            <Link></Link>
+        </div>
+    );
+}
