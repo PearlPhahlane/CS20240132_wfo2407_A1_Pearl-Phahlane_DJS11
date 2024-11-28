d19dc83ff174660ee4bd1430ea9f04e05cb14cd0
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-29 00:26:48 +0200

    import AudioPlayerProvider in App.jsx and wrap application with the provider

--- cast-ring-app/src/App.jsx
+++ cast-ring-app/src/App.jsx
@@ -7,23 +7,28 @@ import Browse from "./pages/Browse";
 import Favorites from "./pages/Favorites";
 import PodcastDetail from "./pages/PodcastDetail";
 import Layout from './components/Layout';
+import AudioPlayerProvider from "./AudioPlayerContex";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Route for landingpage at '/' */}
-        <Route path="/" element={<LandingPage />} />
+      <AudioPlayerProvider>
+        {" "}
+        {/* Wrap application with the provider */}
+        <Routes>
+          {/* Route for landingpage at '/' */}
+          <Route path="/" element={<LandingPage />} />
 
-        {/* Wrapped Layout Route for other pages*/}
-        <Route path="/" element={<Layout />}>
-          <Route path="home" element={<Home />} />
-          <Route path="browse" element={<Browse />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="podcast/:podcastId" element={<PodcastDetail />} />
-        </Route>
-      </Routes>
+          {/* Wrapped Layout Route for other pages*/}
+          <Route path="/" element={<Layout />}>
+            <Route path="home" element={<Home />} />
+            <Route path="browse" element={<Browse />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="podcast/:podcastId" element={<PodcastDetail />} />
+          </Route>
+        </Routes>
+      </AudioPlayerProvider>
     </BrowserRouter>
   );
 }
